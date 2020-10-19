412ddf5e7242bf553d716dd8a4ceaa7d1dde1da0
Jason Liew <jason.97.liew@gmail.com>
2020-10-19 21:23:33 +1100

    Fix bug where nearest whole bpm was not showing

--- src/components/BPMDisplay.js
+++ src/components/BPMDisplay.js
@@ -10,7 +10,7 @@ const BPMDisplay = ({ bpm }) => {
 				</tr>
 				<tr>
 					<td>Nearest Whole</td>
-					<td><b>{(typeof(bpm) === 'number') ? Math.round(bpm) : ''}</b></td>
+					<td><b>{(!isNaN(bpm)) ? Math.round(bpm) : ''}</b></td>
 				</tr>
 			</tbody>
 		</table>
